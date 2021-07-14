fc7e4b7abfd73b47fb81b6f771c422b2dc452f25
Jackson Merma <jmermap@unsa.edu.pe>
2021-07-14 18:56:09 -0500

    seguridad de formularios y guardado correcto

--- miProyecto/personas/forms.py
+++ miProyecto/personas/forms.py
@@ -14,6 +14,6 @@ class PersonaForm(forms.Form):
 
 class RawPersonaForm(forms.Form):
     nombres=forms.CharField()
-    appelidos=forms.CharField()
+    apellidos=forms.CharField()
     edad=forms.IntegerField()
     donador=forms.BooleanField()
--- miProyecto/personas/views.py
+++ miProyecto/personas/views.py
@@ -23,7 +23,15 @@ def personaCreateView(request):
     return render(request, 'personasCreate.html', context)
 
 def personaAnotherCreateView(request):
-    form=RawPersonaForm(request.POST)
+    form=RawPersonaForm()
+    if(request.method=='POST'):
+        form=RawPersonaForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            Persona.objects.create(**form.cleaned_data)
+        else:
+            print(form.errors)
+
     context={
         'form':form
     }
