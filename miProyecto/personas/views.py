57b17449a69a1228ab0c80ba334e7f6274256440
Jackson Merma <jmermap@unsa.edu.pe>
2021-07-14 19:19:25 -0500

    objeto no encontrado

--- miProyecto/personas/views.py
+++ miProyecto/personas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import  Persona
 from .forms import PersonaForm, RawPersonaForm
 
@@ -51,7 +51,7 @@ def searchForHelp(request):
     return render(request, 'search.html', {})
 
 def personasShowObject(request, myID):
-    obj=Persona.objects.get(id=myID)
+    obj=get_object_or_404(Persona,id=myID)
     context={
         'objeto':obj
     }
